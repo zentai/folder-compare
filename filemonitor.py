f4e76ce2ac0377eeb358f904e0e7ae93e95c68fd
zen.tai <walao81@gmail.com>
2012-01-17 08:11:02 +0800

    refactory compare code into class DiffScanner and FolderScanner append remove file compare

--- filemonitor.py
+++ filemonitor.py
@@ -3,11 +3,34 @@ import time
 import filecmp
 import hashlib
 import ast
+import logging
+logging.basicConfig(
+    level = logging.INFO,
+    format = '%(asctime)s %(levelname)s %(module)s.%(funcName)s():%(lineno)s %(message)s',
+)
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+hdlr = logging.FileHandler('info.log')
+format = '%(asctime)s %(levelname)s %(module)s.%(funcName)s():%(lineno)s %(message)s',
+format = '%(asctime)s %(levelname)s %(message)s'
+formatter = logging.Formatter(format)
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr) 
+
+
 SNAPSHOT = "\\files.snapshot"
 
+class FolderNotExistsException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+        
 class FileMeta(object):
     """
-    FileMeta - store the file information.
+    FileMeta - build up the file snapshot by sha-1.
     """
     def __init__(self, full, base, file, size):
         self.full = full + '\\' + file
@@ -26,54 +49,89 @@ class FileMeta(object):
             sha1.update(f.read())
         finally:
             f.close()
+        logger.debug("%s - %s" % (sha1.hexdigest(), filepath))
         return sha1.hexdigest()
 
-        
-def scan(folder='.'):
-    file_versions = {}
-    rootlen = len(folder)
-    
-    # lookup snapshot before scan
-    if os.path.exists(folder + SNAPSHOT):
-        fmeta = open(folder + SNAPSHOT, 'r')
-        file_version = ast.literal_eval(fmeta.read())
-        fmeta.close()
-        return file_version
-    
-    i = 0
-    for base, dirs, files in os.walk(folder):
-        for file in files:
-            (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(base+'/'+file)
-            file_meta = FileMeta(base, base.replace(folder+'\\', ''), file, size)
-            file_versions[file_meta.relative_path] = file_meta.hash
-    
-    # store snapshot after scan
-    fmeta = open(folder + SNAPSHOT, 'w')            
-    fmeta.write(str(file_versions))
-    fmeta.close()
-    
-    return file_versions
-    
-def diff_list(version_old, version_new):
-    f = open('change_list.txt', 'w')
-    diff = {}
-    for key, value in sorted(version_new.items()):
-        new_file_hash = value
+class DiffScanner(object):
+    """
+    compare 2 version folder, look up difference. 
+    """
+    def __init__(self, new_version, old_version):
         try:
-            old_file_hash = version_old[key]
-            if new_file_hash != old_file_hash:
+            logger.info("Compare: %s, %s" % (new_version, old_version))
+            self.new_version = FolderScanner(new_version).scan()
+            self.old_version = FolderScanner(old_version).scan()
+        except FolderNotExistsException as e:
+            raise e
+
+    def scan(self):
+        f = open('change_list.txt', 'w')
+        diff = {}
+        for key, value in sorted(self.new_version.items()):
+            new_file_hash = value
+            try:
+                old_file_hash = self.old_version[key]
+                if new_file_hash != old_file_hash:
+                    diff[key] = value
+                    s = "%s %s \n" % ('[U]', key)
+                    logger.debug(s)
+                    f.write(s)
+                else:
+                    pass
+                    #s = "%s %s \n" % ('[ ]', key)
+                    #f.write(s)
+            except KeyError:
                 diff[key] = value
-                s = "%s \t %s \n" % (key, ' changed')
+                s = "%s %s \n" % ('[+]', key)
+                logger.debug(s)
                 f.write(s)
-            else:
-                s = "%s \t %s \n" % (key, '')
+        
+        for key in sorted(self.old_version):
+            if not key in self.new_version:
+                s = "%s %s \n" % ('[-]', key)
+                logger.debug(s)
                 f.write(s)
-        except KeyError:
-            diff[key] = value
-            s = "%s \t %s \n" % (key, ' new file')
-            f.write(s)
-    f.close()
-    return diff
+        
+        f.close()
+        logger.info("Compare completed")
+        
+        return diff
+        
+class FolderScanner(object):
+    def __init__(self, folder):
+        self.folder = folder
+        if not os.path.exists(folder):
+            msg = "%s not found" % folder
+            logger.error(msg)
+            raise FolderNotExistsException(msg)
+        logger.info("scan folder: %s" % folder)
+        
+    def scan(self):
+        file_versions = {}
+        rootlen = len(self.folder)
+        
+        # lookup snapshot before scan
+        
+        if os.path.exists(self.folder + SNAPSHOT):
+            fmeta = open(self.folder + SNAPSHOT, 'r')
+            file_version = ast.literal_eval(fmeta.read())
+            fmeta.close()
+            logger.debug("folder: %s , scan mode: %s" % (self.folder, "snapshot"))
+            return file_version
+        logger.debug("folder: %s , scan mode: %s" % (self.folder, "deep"))
+        i = 0
+        for base, dirs, files in os.walk(self.folder):
+            for file in files:
+                (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(base+'/'+file)
+                file_meta = FileMeta(base, base.replace(self.folder+'\\', ''), file, size)
+                file_versions[file_meta.relative_path] = file_meta.hash
+        # store snapshot after scan
+        fmeta = open(self.folder + SNAPSHOT, 'w')            
+        fmeta.write(str(file_versions))
+        fmeta.close()
+        logger.info("scan folder: %s completed" % self.folder)
+        return file_versions
+    
 
 def Release(diff):    
     command = ''
@@ -119,15 +177,14 @@ if __name__ == "__main__":
         print "= Default: 3.0.26 D:\\python_project\\FingerPrint\\release D:\\python_project\\FingerPrint\\Genecodev3.0.25" 
         print "========================================="
         release_version = "3.0.26"
-        new_path = 'D:\\python_project\\FingerPrint\\release'
-        old_path = 'D:\\python_project\\FingerPrint\\Genecodev3.0.25'
+        new_path = 'D:\\python_project\\ReleaseRepository\\Genecodev3.0.31'
+        old_path = 'D:\\python_project\\ReleaseRepository\\Genecodev3.0.30'
     else:
         release_version = sys.argv[1]
         new_path = sys.argv[2]
         old_path = sys.argv[3]
-    versions_new = scan(new_path)
-    versions_old = scan(old_path)
-    diff = diff_list(versions_old, versions_new)
-    folder_list = {}
+    x = DiffScanner(new_path, old_path)
+    x.scan()
+    
     #Release(diff)
     #BuildUpdateFile(release_version, diff)
