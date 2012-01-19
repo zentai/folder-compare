5b945c1f831a021c5369eb5b6ae982d340d416d3
zen.tai <walao81@gmail.com>
2012-01-19 08:01:44 +0800

    refactory filemonitor

--- filemonitor.py
+++ filemonitor.py
@@ -21,7 +21,7 @@ logger.addHandler(hdlr)
 
 
 SNAPSHOT = "\\files.snapshot"
-
+CHANGELOG = "change_list.log"
 class FolderNotExistsException(Exception):
     def __init__(self, value):
         self.value = value
@@ -65,14 +65,14 @@ class DiffScanner(object):
             raise e
 
     def scan(self):
-        f = open('change_list.txt', 'w')
+        f = open(CHANGELOG, 'w')
         diff = {}
         for key, value in sorted(self.new_version.items()):
             new_file_hash = value
             try:
                 old_file_hash = self.old_version[key]
                 if new_file_hash != old_file_hash:
-                    diff[key] = value
+                    diff[key] = "U"
                     s = "%s %s \n" % ('[U]', key)
                     logger.debug(s)
                     f.write(s)
@@ -81,13 +81,14 @@ class DiffScanner(object):
                     #s = "%s %s \n" % ('[ ]', key)
                     #f.write(s)
             except KeyError:
-                diff[key] = value
+                diff[key] = "+"
                 s = "%s %s \n" % ('[+]', key)
                 logger.debug(s)
                 f.write(s)
         
         for key in sorted(self.old_version):
             if not key in self.new_version:
+                diff[key] = "-"
                 s = "%s %s \n" % ('[-]', key)
                 logger.debug(s)
                 f.write(s)
@@ -98,6 +99,9 @@ class DiffScanner(object):
         return diff
         
 class FolderScanner(object):
+    """
+    scan specify folder and calc each file sha-1.
+    """
     def __init__(self, folder):
         self.folder = folder
         if not os.path.exists(folder):
@@ -105,7 +109,13 @@ class FolderScanner(object):
             logger.error(msg)
             raise FolderNotExistsException(msg)
         logger.info("scan folder: %s" % folder)
-        
+
+    """
+    snapshot = True
+    lookup files.snapshot before rescan all files.
+    snapshot = False
+    rescan all file and rebuild files.snapshot
+    """
     def scan(self, snapshot = True):
         file_versions = {}
         rootlen = len(self.folder)
@@ -125,66 +135,25 @@ class FolderScanner(object):
                 (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(base+'/'+file)
                 file_meta = FileMeta(base, base.replace(self.folder, ''), file, size)
                 file_versions[file_meta.relative_path] = file_meta.hash
-        # store snapshot after scan
+        # build files.snapshot after scan
         fmeta = open(self.folder + SNAPSHOT, 'w')            
         fmeta.write(str(file_versions))
         fmeta.close()
         logger.info("scan folder: %s completed" % self.folder)
         return file_versions
-    
-
-def Release(diff):    
-    command = ''
-    for key, value in diff.items():
-        #print "size: %(size)s \t reason: %(reason)s \t %(file)s \t folder: %(folder)s" % {'file': key , 'size': value.size, 'folder': value.base, 'reason': value.reason}
-        st = value.pack()
-        folder_list[value.base] = value
-        command += st+'\n'
-    
-    mkdirs = ''
-    mkdirs += 'RD /S release_pack /Q \n'
-    for key in folder_list:
-        mkdirs += 'mkdir %s\\%s \n' % ('release_pack', key)
-        
-    release_bat = file('release.bat', 'w')
-    release_bat.write(mkdirs)
-    release_bat.write(command)
-    release_bat.write("move update.bat release_pack\\dist\\")
-    release_bat.close()
-
-def BuildUpdateFile(release_version, diff):
-    command = ''
-    for key, value in diff.items():
-        #print "size: %(size)s \t reason: %(reason)s \t %(file)s \t folder: %(folder)s" % {'file': key , 'size': value.size, 'folder': value.base, 'reason': value.reason}
-        st = value.save(release_version)
-        folder_list[value.base] = value
-        command += st+'\n'
-    
-    mkdirs = ''
-    for key in folder_list:
-        mkdirs += 'mkdir %s \n' % key
-        
-    update_bat = file('update.bat', 'w') 
-    update_bat.write(mkdirs)
-    update_bat.write(command)
-    update_bat.close()
 
 import sys    
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 3:
         print "========================================="
         print "= Usage: d:\\filemonitor.py new_version_path old_version_path" 
-        print "= Default: 3.0.26 D:\\python_project\\FingerPrint\\release D:\\python_project\\FingerPrint\\Genecodev3.0.25" 
+        print "= Default: filemonitor.py D:\\python_project\\FingerPrint\\release D:\\python_project\\FingerPrint\\Genecodev3.0.25"
         print "========================================="
-        release_version = "3.0.26"
         new_path = 'D:\\python_project\\ReleaseRepository\\Genecodev3.0.31'
         old_path = 'D:\\python_project\\ReleaseRepository\\Genecodev3.0.30'
     else:
-        release_version = sys.argv[1]
-        new_path = sys.argv[2]
-        old_path = sys.argv[3]
-    x = DiffScanner(new_path, old_path, snapshot = False)
+        new_path = sys.argv[1]
+        old_path = sys.argv[2]
+    x = DiffScanner(new_path, old_path, snapshot = True)
     x.scan()
-    
-    #Release(diff)
-    #BuildUpdateFile(release_version, diff)
+    os.system("start " + CHANGELOG)
