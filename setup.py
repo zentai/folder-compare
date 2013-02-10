f8683b48e1e4e6825c1b5ec25d01a5bb767d3462
ZEN <zen@ctrlworks.com>
2013-02-10 16:12:28 +0800

    add setup.py for release to pypi

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'foldercompare',
+    version = '1.0.0',
+    py_modules = ['foldercompare'],
+    author = 'Zen',
+    author_email = 'walao81@gmail.com',
+    url = 'https://github.com/zentai/folder-compare',
+    description = 'just a easy tools to compare 2 folder and generated a files changed list for you.',
+)
